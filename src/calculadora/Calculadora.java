34a56162bfb1b9552a9408a14e78a91026f7faa3
Cesar Cueva <cueva1487@gmail.com>
2019-07-03 16:15:00 -0500

    before y after

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -2,23 +2,36 @@ package calculadora;
 
 public class Calculadora {
 
-	public static int suma(int a, int b) {
-		return a+b;
-	}
-	public static int resta(int a, int b ) {
-		return a-b;
+	public int ans;
+	
+	public Calculadora() {
+		ans = 0;
 	}
 	
-	public static void main(String args[]) {
-		int a=3,b=2;
-		int res= suma(a ,b);
-		int esperado = 5;
-		if(res==esperado) {
-			System.out.println("programa bien echo");
-			
-		}else {
-			System.out.println("programa mal hecho");
-		}
-		
+	public int add(int a, int b) {
+		ans = a + b;
+		return ans;
+	}
+	
+	public int sub(int a, int b) {
+		ans = a - b;
+		return ans;
+	}
+	
+	public int add(int v) {
+		ans += v;
+		return ans;
+	}
+	public int sub(int v) {
+		ans -= v;
+		return ans;
+	}
+	
+	public int ans() {
+		return ans;
+	}
+	
+	public void clear() {
+		ans = 0;
 	}
 }
--- src/calculadora/CalculadoraTest.java
+++ src/calculadora/CalculadoraTest.java
@@ -2,22 +2,39 @@ package calculadora;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculadoraTest {
 
+	
+	Calculadora cal;
+	
+	@Before
+	public void before() {
+		System.out.println("before");
+		cal = new Calculadora();
+	}
+	
+	@After
+	public void after() {
+		System.out.println("After");
+		cal.clear();
+	}
 	@Test
-	public void TestSuma() {
-		int resultado = Calculadora.suma(2, 3);
-		int esperado = 5;
-		assertEquals(esperado, resultado);
+	public void TestSum() {
+		int result = cal.add(3, 2);
+		int esper = 5;
+		assertEquals(esper, result);
 	}
 	
 	@Test
-	public void TestResta() {
-		int resultado = Calculadora.resta(3, 2);
-		int esperado = 1;
-		assertEquals(esperado, resultado);
+	public void TestAnsSum() {
+		cal.add(3, 2);
+		int resul = cal.ans();
+		int esper = 5;
+		assertEquals(esper, resul);
 	}
 
 }
