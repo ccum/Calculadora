6d18b2c57c179d1096eb228af3ad4b6f1f74a6fe
Cesar Cueva <cueva1487@gmail.com>
2019-07-03 15:13:16 -0500

    agregando mi primer test

---
+++ src/calculadora/CalculadoraTest.java
@@ -0,0 +1,23 @@
+package calculadora;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+	@Test
+	public void TestSuma() {
+		int resultado = Calculadora.suma(2, 3);
+		int esperado = 5;
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	public void TestResta() {
+		int resultado = Calculadora.resta(3, 2);
+		int esperado = 1;
+		assertEquals(esperado, resultado);
+	}
+
+}
