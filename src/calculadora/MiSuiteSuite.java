44d82d92d829d1111ae14cd810ef4a228ae97550
Cesar Cueva <cueva1487@gmail.com>
2019-07-04 19:44:59 -0500

    Suites

---
+++ src/calculadora/MiSuiteSuite.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value=Suite.class)
+@SuiteClasses({
+	CalculadoraTest.class,
+	CalculadoraParametroTest.class
+})
+public class MiSuiteSuite {
+
+}
